12c552c373495f1c39833be396c0960d12fa47dd
Gabinho <gbenincasa042@gmail.com>
2026-05-30 21:41:45 -0300

    se arreglo el problema que el comando task-cli update no modificaba la tarea

--- taskcli.py
+++ taskcli.py
@@ -1,7 +1,6 @@
 import argparse
 from datetime import datetime
 import shlex
-from tkinter import S
 from typing import Optional
 from pydantic import BaseModel , Field
 
@@ -31,20 +30,20 @@ def comandos_entrada_parser(data: Data):
 
     #task-li update 1 "tarea actualizada"
     update_parser = subparser.add_parser("update", help = "actualiza una tarea de la lista")
-    update_parser.add_argument("id", help = "escribir el id de la tarea a actualizar")
+    update_parser.add_argument("id",type=int ,help = "escribir el id de la tarea a actualizar")
     update_parser.add_argument("descripcion", help = "escribir la nueva descripcion de la tarea")
 
     #task-cli delete 1
     delete_parser = subparser.add_parser("delete", help = "elimina una tarea de la lista")
-    delete_parser.add_argument("id", help = "escribir el id de la tarea a eliminar")
+    delete_parser.add_argument("id",type=int , help = "escribir el id de la tarea a eliminar")
 
     #task-cli mark-in-progress 1
     mark_in_progress_parser = subparser.add_parser("mark-in-progress", help = "marca una tarea como en progreso")
-    mark_in_progress_parser.add_argument("id", help = "escribir el id de la tarea a marcar como en progreso")
+    mark_in_progress_parser.add_argument("id",type=int , help = "escribir el id de la tarea a marcar como en progreso")
 
     #task-cli mark-done 1
     mark_done_parser = subparser.add_parser("mark-done", help = "marca una tarea como hecha")
-    mark_done_parser.add_argument("id", help = "escribir el id de la tarea a marcar como hecha")
+    mark_done_parser.add_argument("id",type=int , help = "escribir el id de la tarea a marcar como hecha")
 
     return parser
 
@@ -66,11 +65,10 @@ def modificar_tarea(id: int, descripcion: str):
         if tarea.id == id:
             tarea.description = descripcion
             tarea.updatedAt = datetime.now()
-            index = false_db[tarea].index
-            false_db[index]
-            print(false_db[index])
+            false_db[id-1] = tarea
+            print(false_db[id-1])
             return tarea
-
+    print(false_db[id-1])
 
 def taskcli():
 
@@ -90,7 +88,7 @@ def taskcli():
         elif args.comando == "update":
 
             tar = modificar_tarea(args.id, args.descripcion)
-            print(false_db)
+            #print(false_db)
 
 taskcli()
     
